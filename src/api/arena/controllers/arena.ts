37d47bf4cb55216abf9baaeda7c9e79489daf56e
Ryan Fisch <ryan@end-labs.io>
2022-11-29 09:24:50 -0500

    init

---
+++ src/api/arena/controllers/arena.ts
@@ -0,0 +1,7 @@
+/**
+ * arena controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::arena.arena');
---
+++ src/api/arena/routes/arena.ts
@@ -0,0 +1,7 @@
+/**
+ * arena router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::arena.arena');
